509fd4cc2654210dfbe560375749c1203eb9143a
Raajn Patel <rndpatel@gmail.com>
2019-09-16 15:49:42 -0700

    testing fire event

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from "react";
+
+import { fireEvent, render } from "@testing-library/react";
+import Controls from "./Controls";
+
+it("should invoke a function when unlock or lock gate is clicked", () => {
+    const toggleLocked = jest.fn();
+    const locked = true;
+    const closed = true;
+
+    const { getByText } = render(
+        <Controls toggleLocked={toggleLocked} locked={locked} closed={closed} />
+    );
+    fireEvent.click(getByText("Unlock Gate"));
+
+    expect(toggleLocked).toHaveBeenCalled();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+describe("<Display />", () => {
+    it("renders without crashing", () => {
+        render(<Display />);
+    });
+});
